5ab5d3d2ae67a1956c4084e707d1b73d3d315aef
TEJA RAMIDI <140337408+Tejaramidi0118@users.noreply.github.com>
2025-09-07 03:22:45 +0530

    Time: 3 ms (97.78%), Space: 18.1 MB (85.12%) - LeetHub

--- 0720-longest-word-in-dictionary/0720-longest-word-in-dictionary.py
+++ 0720-longest-word-in-dictionary/0720-longest-word-in-dictionary.py
@@ -1,38 +1,13 @@
-class TrieNode:
-    def __init__(self):
-        self.children = {}
-        self.is_end_of_word = False
-
-class Trie:
-    def __init__(self):
-        self.root = TrieNode()
-
-    def insert(self, word: str):
-        node = self.root
-        for ch in word:
-            if ch not in node.children:
-                node.children[ch] = TrieNode()
-            node = node.children[ch]
-        node.is_end_of_word = True
-
 class Solution:
     def longestWord(self, words: List[str]) -> str:
-        trie = Trie()
+        words.sort()
+        built = set([""])
+        longest = ""
+
         for word in words:
-            trie.insert(word)
+            if word[:-1] in built:
+                built.add(word)
+                if len(word) > len(longest):
+                    longest = word
 
-        ans = [""]
-        temp = []
-
-        def dfs(node):
-            for ch in sorted(node.children.keys()):
-                child = node.children[ch]
-                if child.is_end_of_word:
-                    temp.append(ch)
-                    if len(temp) > len(ans[0]):
-                        ans[0] = "".join(temp)
-                    dfs(child)
-                    temp.pop()
-
-        dfs(trie.root)
-        return ans[0]
+        return longest

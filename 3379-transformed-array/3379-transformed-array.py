341f4141e4fba03edce73509d62786a51522b8e7
TEJA RAMIDI <140337408+Tejaramidi0118@users.noreply.github.com>
2026-02-08 10:56:18 +0530

    Time: 63 ms (55.29%), Space: 19.3 MB (85.43%) - LeetHub

---
+++ 3379-transformed-array/3379-transformed-array.py
@@ -0,0 +1,12 @@
+class Solution:
+    def constructTransformedArray(self, nums: List[int]) -> List[int]:
+        
+        result = []
+
+        for i in range(len(nums)):
+            if nums[i] == 0:
+                result.append(nums[i])
+            else:
+                result.append(nums[(i + nums[i]) % len(nums)])
+        
+        return result

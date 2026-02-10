80d9c17a3b120c8fde08b329c4f4758cf584fc1d
TEJA RAMIDI <140337408+Tejaramidi0118@users.noreply.github.com>
2026-02-10 22:27:25 +0530

    Time: 238 ms (86.03%), Space: 65.6 MB (91.26%) - LeetHub

--- 2877-create-a-dataframe-from-list/2877-create-a-dataframe-from-list.py
+++ 2877-create-a-dataframe-from-list/2877-create-a-dataframe-from-list.py
@@ -1,9 +1,10 @@
 import pandas as pd
 
 def createDataframe(student_data: List[List[int]]) -> pd.DataFrame:
-    df = pd.DataFrame({
-        "student_id": [id for id, _ in student_data], 
-        "age": [age for _, age in student_data]
-    })
+    # df = pd.DataFrame({
+    #     "student_id": [id for id, _ in student_data], 
+    #     "age": [age for _, age in student_data]
+    # })
     
-    return df
+    # return df
+    return pd.DataFrame(student_data, columns=['student_id', 'age'])

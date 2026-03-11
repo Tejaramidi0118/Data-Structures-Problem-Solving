9a7d80d39a8a35701f64cb1cac68d82f5535d87c
TEJA RAMIDI <140337408+Tejaramidi0118@users.noreply.github.com>
2026-03-11 19:11:19 +0530

    Time: 0 ms (100%), Space: 19.3 MB (58.78%) - LeetHub

---
+++ 1009-complement-of-base-10-integer/1009-complement-of-base-10-integer.py
@@ -0,0 +1,38 @@
+class Solution:
+    def bitwiseComplement(self, n: int) -> int:
+        if n == 0:
+            return 1
+
+        def revbits(s):
+            rev = ""
+            for bit in s:
+                if bit == '1':
+                    rev += '0'
+                else:
+                    rev += '1'
+            
+            return rev
+        
+        def num2bin(n):
+            s = ""
+
+            while n > 0:
+                s = str(n % 2) + s
+
+                n //= 2
+            
+            return s
+        
+        def bin2num(s):
+            n = 0
+            power = 0
+
+            for i in range(len(s)-1,-1,-1):
+                if s[i] == '1':
+                    n += 2 ** power
+                
+                power += 1
+
+            return n
+
+        return bin2num(revbits(num2bin(n)))

e6de11184a0ae3188fdb633c7c3c8f56ff9c3f3c
TEJA RAMIDI <140337408+Tejaramidi0118@users.noreply.github.com>
2026-04-17 22:40:22 +0530

    Time: 200 ms (92.64%), Space: 41.9 MB (73.64%) - LeetHub

---
+++ 4139-minimum-absolute-distance-between-mirror-pairs/4139-minimum-absolute-distance-between-mirror-pairs.py
@@ -0,0 +1,11 @@
+class Solution:
+    def minMirrorPairDistance(self, nums: List[int]) -> int:
+        res = float('inf')
+        seen = {}
+
+        for i, n in enumerate(nums):
+            if n in seen:
+                res = min(res, i - seen[n])
+            seen[int(str(n)[::-1])] = i
+
+        return -1 if res == float('inf') else res

adb7d256019b07657fd7685810d5ae047eaabbeb
TEJA RAMIDI <140337408+Tejaramidi0118@users.noreply.github.com>
2026-06-14 13:39:32 +0530

    Time: 0 ms (100%), Space: 19.3 MB (79.53%) - LeetHub

---
+++ 1603-running-sum-of-1d-array/1603-running-sum-of-1d-array.py
@@ -0,0 +1,10 @@
+class Solution:
+    def runningSum(self, nums: List[int]) -> List[int]:
+        preSum = [0 for _ in range(len(nums ))]
+
+        preSum[0] = nums[0]
+
+        for i in range(1,len(nums)):
+            preSum[i] = preSum[i-1] + nums[i]
+        
+        return preSum

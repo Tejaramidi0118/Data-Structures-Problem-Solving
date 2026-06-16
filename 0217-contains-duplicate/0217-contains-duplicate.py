6a377fa3475622b94490fd5198af1fafd351b6d9
TEJA RAMIDI <140337408+Tejaramidi0118@users.noreply.github.com>
2026-06-16 20:09:48 +0530

    Time: 8 ms (78.45%), Space: 31.4 MB (54.02%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -0,0 +1,10 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        a = set(nums)
+        
+        if len(a)!= len(nums):
+            return True
+        else:
+            return False
+    
+        
